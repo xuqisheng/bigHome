d335c77281d86b758aa16051c42a4fd8b424658d
Zhang94mz817 <18516654426@163.com>
2018-11-05 16:25:57 +0800

    config

--- utils/require.js
+++ utils/require.js
@@ -8,12 +8,11 @@ function wxGetData (url, data, method) {
       data: postData,
       method: method,
       header: { 'content-type': 'application/json' },
-      success: function (res) {//服务器返回数据
-        if (res.data.status == 1) {
-          //res.data 为 后台返回数据，格式为{"data":{...}, "info":"成功", "status":1}, 后台规定：如果status为1,既是正确结果。可以根据自己业务逻辑来设定判断条件
-          resolve(res.data.data);
-        } else {                //返回错误提示信息
-          reject(res.data.info);
+      success: function (res) {
+        if (res.statusCode) {
+          resolve(res);
+        } else {                
+          reject(res+'错误');
         }
       },
       error: function (e) {
