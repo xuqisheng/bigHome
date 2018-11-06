381c3ab75ab57facfa5bd7b74e015dad7ec587df
Zhang94mz817 <18516654426@163.com>
2018-11-06 11:30:59 +0800

    change html

--- pages/yk/index.js
+++ pages/yk/index.js
@@ -8,10 +8,13 @@ Page({
     sliderOffset: 0,
     sliderLeft: 0,
     localInfoBottom: 0,
-    showCancelBox:0
+    showCancelBox:0,
+    contactState:2,
+    states:['待看房','已看房','已取消'],
+    colors: ['#B5B5B5', '#00ADA9','#ff4545']
   },
   onLoad: function () {
-    var that = this;
+    var that = this; 
     var query = wx.createSelectorQuery();
     query.select('#local_info').boundingClientRect()
     query.exec(function (res) {
@@ -38,12 +41,13 @@ Page({
   },
   ceCancel:function(e){
     this.setData({
-      showCancelBox: 0
+      showCancelBox: 0,
+      contactState: 2
     })
   },
   caCancel: function (e) {
     this.setData({
-      showCancelBox: 0
+      showCancelBox: 0,
     })
     // requierjs.wxGetData()
     // .then((res) => {
@@ -53,7 +57,26 @@ Page({
   },
   ceyy:function(e){
     this.setData({
-      showCancelBox : 1
+      showCancelBox : 1,
+    })
+  },
+  ccontactState:function(e){
+    this.setData({
+      contactState:0
+    })
+  },
+  mpc:function(e){
+    wx.makePhoneCall({
+      phoneNumber: '1340111000' //仅为示例，并非真实的电话号码
+    })
+  },
+  storeNavigation:function(e){
+    wx.openLocation({
+      latitude: 23.362490,
+      longitude: 116.715790,
+      scale: 18,
+      name: '华乾大厦',
+      address: '金平区长平路93号'
     })
   }
 });
