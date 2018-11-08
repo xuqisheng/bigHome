2864ddce9207df01dddc0d43e770e502e217e571
osjj <597167998@qq.com>
2018-11-08 17:52:10 +0800

    update

---
+++ utils/mapMock.js
@@ -0,0 +1,72 @@
+const mapData = [{
+    "id": 0,
+    "name": "BIG+碧家国际社区深圳坂田店",
+    "latitude": 23.099994,
+    "longitude": 113.324520,
+  },
+  {
+    "id": 1,
+    "name": "BIG+碧家国际社区深圳坂田2店",
+    "latitude": 23.13171,
+    "longitude": 113.26627,
+  },
+  {
+    "id": 2,
+    "name": "BIG+碧家国际社区深圳坂田3店",
+    "latitude": 25.069694,
+    "longitude": 113.225520,
+  },
+  {
+    "id": 3,
+    "name": "BIG+碧家国际社区深圳坂田3店",
+    "latitude": 25.069694,
+    "longitude": 113.225520,
+  },
+  {
+    "id": 4,
+    "name": "BIG+碧家国际社区深圳坂田3店",
+    "latitude": 25.069694,
+    "longitude": 113.225520,
+  }
+]
+
+const mapImageData = [{
+  "id": 0,
+  "imageSrc": "../../image/home_page/1.png",
+  "bookStatus":true,
+  "name": "BIG+碧家国际社区深圳坂田店", 
+  "rent":"¥1300元 / 月起",
+  "configure": ["地铁附近", "配套完善", "交通便利","家电齐全"]
+},{
+    "id": 1,
+    "imageSrc": "../../image/home_page/5.png",
+    "bookStatus": false,
+    "name": "BIG+碧家国际社区深圳坂田2店",
+    "rent": "¥2900元/月起",
+    "configure": ["地铁附近", "配套完善", "交通便利", "家电齐全"]
+},{
+    "id": 2,
+    "imageSrc": "../../image/home_page/6.png",
+    "bookStatus": true,
+    "name": "BIG+碧家国际社区深圳坂田3店", 
+    "rent": "¥2300元 / 月起",
+    "configure": ["地铁附近", "配套完善", "交通便利", "家电齐全"]
+},{
+    "id": 3,
+    "imageSrc": "../../image/home_page/7.png",
+    "bookStatus": false,
+    "name": "BIG+碧家国际社区深圳坂田4店",
+    "rent": "¥4300元 / 月起",
+    "configure": ["地铁附近", "配套完善", "交通便利", "家电齐全"]
+  }, {
+    "id": 4,
+    "imageSrc": "../../image/home_page/1.png",
+    "bookStatus": false,
+    "name": "BIG+碧家国际社区深圳坂田5店",
+    "rent": "¥8300元 / 月起",
+    "configure": ["地铁附近", "配套完善", "交通便利", "家电齐全"]
+  }]
+  module.exports={
+    mapData,
+    mapImageData
+  }
