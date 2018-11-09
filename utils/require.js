7f34f294a50be3291e0666198d6b010aba82da7b
Zhang94mz817 <18516654426@163.com>
2018-11-09 17:02:06 +0800

    1

--- utils/require.js
+++ utils/require.js
@@ -1,5 +1,13 @@
 // Http请求
 // obj= {url:'', data:'', method:'',token:''}
+/*require.wxGetData (obj).then( (res)=>{
+ console.log(res);//正确返回结果
+  //其他操作
+      } ).catch ( (errMsg) => {
+   console.log(errMsg);//错误提示信息
+  //其他操作
+ } );
+*/
 function wxGetData (obj) {
   var promise = new Promise((resolve, reject) => {
     var that = this;
