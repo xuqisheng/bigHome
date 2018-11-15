51b1a7fa5a2060376eaa9ee31de3861016c05b04
osjj <597167998@qq.com>
2018-11-15 08:58:19 +0800

    update

--- pages/housingResources_page/index.js
+++ pages/housingResources_page/index.js
@@ -1,70 +1,3 @@
-<<<<<<< HEAD
-// pages/housingResources_page/index.js
-Page({
-
-  /**
-   * 页面的初始数据
-   */
-  data: {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
-  }
-=======
 var rq = require("../../utils/require.js")
 var mlist = [[-1,-1],[-1,2000],[2000,3500],[3500,5000],[5000,-1]]
 var slist = [['price', 1], ['price', 2], ['distance',1]]
@@ -332,6 +265,5 @@ Page({
       let s1 = s<3? slist[s][0] : ''
       let s2 = s<3? slist[s][1] : ''
       this.getHotelList(p1,p2,s1,s2)
-  },
->>>>>>> 1b75e634c4ad764cf1ac79c2f07282e20c51e8d9
+  }
 })
