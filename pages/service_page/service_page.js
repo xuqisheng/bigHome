118cf50a0b3289cfa9ab597378c291b67ad8d1a5
junjunlius <3031488867@qq.com>
2018-11-16 08:34:52 +0800

    123

--- pages/service_page/service_page.js
+++ pages/service_page/service_page.js
@@ -68,5 +68,17 @@ Page({
     wx.navigateTo({
       url: '../complaint_page/complaint_page',
     })
+  },
+  // 客服电话拨打
+    calling:function(){
+    wx.makePhoneCall({
+      phoneNumber: '400-0000-887', //此号码并非真实电话号码，仅用于测试
+      success: function () {
+        console.log("拨打电话成功！")
+      },
+      fail: function () {
+        console.log("拨打电话失败！")
+      }
+    })
   }
 })
