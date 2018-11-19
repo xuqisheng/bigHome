7fe486acdfef48e04e97f7c97d6b367b107c4786
Zhang94mz817 <18516654426@163.com>
2018-11-19 19:00:51 +0800

    1

--- pages/storeDetails_page/index.js
+++ pages/storeDetails_page/index.js
@@ -1,35 +1,32 @@
 // pages/storeDetails_page/storeDetails_page.js
+var rq = require("../../utils/require.js")
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    swiper: {
-      // banner
-      imgUrls: [{
-        imgs: "../../images/home_page/1.png",
-          title: "合租、整租分散式房源",
-        },
-        {
-          imgs: "../../images/home_page/7.png",
-          title: "￥2900/月起",
-        },
-        {
-          imgs: "../../images/home_page/1.png",
-          title: "￥2090/月起",
-        },
-      ]
-    },
-    swiperCurrent: 0,//获取轮播图的下标,
-    current:3
+    current:0,
+    drop1:0,
+    drop2: 0,
+    showAll:false,//过渡蒙版
+    showError:false,//请求错误
+    id:'',
+    house:[],
+    markers: [{
+      id: 0,
+      latitude: '',
+      longitude: '',
+      width: 33,
+      height: 46
+    }],
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function(options) {
-
+    this.getHotelInfo(this.data.id)
   },
 
   /**
@@ -87,5 +84,71 @@ Page({
       swiperCurrent: e.detail.current   //获取当前轮播图片的下标
     })
     console.log(this.data.swiperCurrent);
+  },
+  service_drop:function(e){
+    let d = !this.data.drop1
+    this.setData({
+      drop1 : d
+    })
+  },
+  type_drop: function (e) {
+    let d = !this.data.drop2
+    this.setData({
+      drop2: d
+    })
+  },
+  getHotelInfo: function (e) {
+    let that = this
+    let id = e ? e : 64
+    let obj = {
+      url: 'http://bgy.h-world.com/api/hotel/getHotelDetail',
+      data: {
+        hotelId : id
+      },
+      method: 'POST',
+      isMock: true
+    }
+    rq.wxGetData(obj).then((res) => {
+      if (res.statusCode == 200) {
+        let la = 'this.data.markers.latitude'
+        let lo = 'this.data.markers.longitude'
+        that.setData({
+          house:res.data.data.detail,
+          [lo]: res.data.data.detail.longitude,
+          [la]: res.data.data.detail.latitude,
+          showError: false,
+        })
+        console.log(res.data.data.detail)
+        console.log(this.data.markers)
+        setTimeout(function () {
+          that.setData({
+            showAll: true
+          })
+        }, 200)
+      } else {
+        that.setData({
+          showAll: true,
+          showError: true
+        })
+        wx.showToast({
+          title: '加载失败！',
+          mask: true,
+          icon: 'none',
+          duration: 1000
+        })
+      }
+    }).catch((errMsg) => {
+      that.setData({
+        showAll: true,
+        showError: true
+      })
+      wx.showToast({
+        title: '加载失败！',
+        mask: true,
+        icon: 'none',
+        duration: 1000
+      })
+      console.log(errMsg);
+    });
   },
 })
